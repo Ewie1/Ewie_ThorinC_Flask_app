efe558031063a5ed89556746719c17e0042c619f
Ewie1 <shephvo12@gmail.com>
2022-12-17 13:13:48 +0000

    Add requirements.txt

--- run.py
+++ run.py
@@ -1,11 +1,9 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
 import os
 import json
 from flask import Flask, render_template, request, flash
 if os.path.exists("env.py"):
     import env
+
 
 app = Flask(__name__)
 app.secret_key = os.environ.get("SECRET_KEY")
@@ -21,18 +19,18 @@ def about():
     data = []
     with open("data/company.json", "r") as json_data:
         data = json.load(json_data)
-    return render_template("about.html", page_title= "About", company=data)
+    return render_template("about.html", page_title="About", company=data)
 
 
 @app.route("/about/<member_name>")
 def about_member(member_name):
-    member = []
+    member = {}
     with open("data/company.json", "r") as json_data:
         data = json.load(json_data)
         for obj in data:
             if obj["url"] == member_name:
                 member = obj
-    return render_template("member.html", member=member)  
+    return render_template("member.html", member=member)
 
 
 @app.route("/contact", methods=["GET", "POST"])
